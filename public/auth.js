502c38729bec32856a358142da9f3234f8f6be18
JIA YUE KAM <kamjiayue487@gmail.com>
2023-06-09 11:45:37 +0800

    Finally got working sign up form

--- public/auth.js
+++ public/auth.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,7 +16,7 @@ const firebaseConfig = {
     messagingSenderId: "616024099867",
     appId: "1:616024099867:web:15c57e18d3cc49e3397cbe",
     measurementId: "G-9EBK7QNX7B"
-};
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -25,16 +25,14 @@ const database = getDatabase(app);
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
 
-//Get the sign up form element
-const signupForm = document.querySelector('#signup-form');
+// Get the signupBtn
+const signupBtn = document.getElementById('signup-button');
 
-//Add an event listener to the form submission
-signupForm.addEventListener('submit', (e) => {
-
-    // get user info
-    var email = signupForm['email'].value;
-    var password = signupForm['password1'].value;
-    var passwordConfirmation = signupForm['password2'].value;
+// When signupBtn is clicked, sign up the user 
+signupBtn.addEventListener('click', function(){
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password1").value;
+    const passwordConfirmation = document.getElementById("password2").value;
 
     //check if passwords match
     if (password != passwordConfirmation) {
@@ -46,21 +44,59 @@ signupForm.addEventListener('submit', (e) => {
         .then((userCredential) => {
             // Signed in 
             const user = userCredential.user;
-            
-            //set(ref(database, 'users/' + user.uid), {
-            //    email: email
-            //})
-
-            alert('user created!');
             // ...
+            alert("user created!");
+            console.log("user created!");
         })
         .catch((error) => {
             const errorCode = error.code;
             const errorMessage = error.message;
-            
-            alert(errorMessage);
+            // ..
+            console.log(errorCode + errorMessage);
         });
+
 });
+
+
+
+
+
+
+
+// //Add an event listener to the form submission
+// signupForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+//     // get user info
+//     var email = signupForm['email'].value;
+//     var password = signupForm['password1'].value;
+//     var passwordConfirmation = signupForm['password2'].value;
+
+//     //check if passwords match
+//     if (password != passwordConfirmation) {
+//         alert("passwords do not match")
+//         return;
+//     }
+
+//     createUserWithEmailAndPassword(auth, email, password)
+//         .then((userCredential) => {
+//             // Signed in 
+//             const user = userCredential.user;
+            
+//             //set(ref(database, 'users/' + user.uid), {
+//             //    email: email
+//             //})
+
+//             alert('user created!');
+//             // ...
+//         })
+//         .catch((error) => {
+//             const errorCode = error.code;
+//             const errorMessage = error.message;
+            
+//             alert(errorMessage);
+//         });
+// });
 
 /*const loginForm = document.querySelector('#signin-form');
 
