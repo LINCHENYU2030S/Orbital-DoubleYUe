9137737eb53c5309dc4515e40b869d6ac6a61b3b
JIA YUE KAM <kamjiayue487@gmail.com>
2023-06-28 01:17:38 +0800

    User can now login / signup by pressing Enter

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 //alert("Yo")
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth, signOut, updatePassword, reauthenticateWithCredential } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getAuth, signOut, updatePassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
