62f63db5ce40d5cb61ee171dbe43372322def3c3
JIA YUE KAM <kamjiayue487@gmail.com>
2023-07-05 23:30:09 +0800

    Debugging and optimizing new code for FireStore DB

--- public/script.js
+++ public/script.js
@@ -13,9 +13,12 @@ import {
     doc,
     setDoc,
     getDoc,
+    getDocs,
     addDoc,
     collection,
-    updateDoc
+    updateDoc,
+    increment,
+    onSnapshot
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
 
@@ -43,10 +46,16 @@ const tabContents = document.querySelectorAll('[data-tab-content]');
 let user = null;
 let userEmail = null;
 let balance = null;
+let numOfStocks = null;
 const topUpWithdrawInput = document.getElementById('top-up-withdraw-input');
 const balanceElement = document.getElementById('balance');
 const topUpBtn = document.querySelector('.top-up-btn');
 const withdrawBtn = document.querySelector('.withdraw-btn');
+let currStockTableCapacity = 20;
+let currStockTableColNumber = 8;
+
+// TRADE
+
 
 // SETTINGS
 const changePasswordBtn = document.getElementById('change-password-button');
@@ -58,7 +67,65 @@ const changePasswordPageConfirmChangesButton = document.getElementById('change-p
 const nextBtn = document.getElementById('guide-next-button');
 const backBtn = document.getElementById('guide-back-button');
 
+async function initializeAndUpdateUser() {
+    // Get User Current Portfolio Balance
+    const portfolioDocRef = doc(db, userEmail, "Portfolio");
+    const docSnap = await getDoc(portfolioDocRef);
+    const docData = await docSnap.data();
+    balance = await docData.balance;
+    numOfStocks = await docData.numOfStocks;
+    balanceElement.innerHTML = "Balance: $" + balance;
+    console.log("user current balance is " + balance);
+}
 
+async function initializePortfolioTable () {
+    const portfolioStockTable = document.querySelector('#portfolio-stock-table tbody');
+    console.log("user current owned number of stocks is " + numOfStocks);
+    const stockColRef = collection(db, userEmail, "Portfolio", "Stocks");
+    const docsSnap = await getDocs(stockColRef);
+
+    for (let i = 0; i < currStockTableCapacity; i++) {
+        portfolioStockTable.insertRow();
+    }
+
+    // Initialize Realtime Listener Object to Update Table whenever there is a change to the database
+    onSnapshot(stockColRef, (snapshot) => {
+        for (let i = 1; i <= currStockTableCapacity; i++) {
+            portfolioStockTable.deleteRow(1);
+        }
+        let rowNumber = 0;
+        snapshot.docs.forEach((doc) => {
+            if (doc.id != "?") {
+                rowNumber++;
+                const docData = doc.data();
+                const row = portfolioStockTable.insertRow(-1);
+                for (let j = 0; j < currStockTableColNumber; j++) {
+                    row.insertCell();
+                }
+                const cells = row.cells;
+                cells[0].innerHTML = rowNumber;
+                cells[1].innerHTML = docData.stock;
+                cells[2].innerHTML = docData.type;
+                cells[3].innerHTML = docData.timeFrame;
+                cells[4].innerHTML = docData.size;
+                cells[5].innerHTML = "[Current Price]";
+                cells[6].innerHTML = docData.price;
+                cells[7].innerHTML = "[Profit / Loss]";
+                console.log("filled row inserted");
+            }
+        });
+        if (numOfStocks < currStockTableCapacity) {
+            for (let i = 0; i < currStockTableCapacity - numOfStocks; i++) {
+                const row = portfolioStockTable.insertRow(-1);
+                row.classList.add("unused");
+                console.log("empty row inserted");
+                for (let j = 0; j < currStockTableColNumber; j++) {
+                    row.insertCell();
+                }
+            }
+        }
+    });
+}
 
 function login(email, password) {
     signInWithEmailAndPassword(auth, email, password)
@@ -67,13 +134,15 @@ function login(email, password) {
             user = userCredential.user;
             console.log("User state is signed in!");
             userEmail = user.email;
-            const docRef = doc(db, userEmail, "Portfolio");
+
             (async () => {
-                const docSnap = await getDoc(docRef);
-                balance = await docSnap.data().balance;
-                balanceElement.innerHTML = "Balance: $" + balance;
-                console.log("userPortfolioBalance == " + balance);
+                // Get User Current Portfolio Balance
+                await initializeAndUpdateUser();
+
+                // Initialize User Portfolio Stock Table (IDK WHETHER THIS THING NEED ASYNC OR NOT !!!!!! ans : NEED)
+                await initializePortfolioTable();
             })();
+
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -124,7 +193,8 @@ let totalWithdraw = 0;
 topUpBtn.addEventListener('click', handleTopUp);
 withdrawBtn.addEventListener('click', handleWithdraw);
 
-function updateBalance(balance) {
+function updateBalance(newBalance) {
+    balance = newBalance;
     balanceElement.textContent = `Balance: $` + balance;
     (async () => {
         const portfolioDocRef = doc(db, userEmail, "Portfolio");
@@ -142,9 +212,8 @@ function handleTopUp() {
         return;
     }
 
-    balance += amount;
     totalTopUp += amount;
-    updateBalance(balance);
+    updateBalance(balance + amount);
     topUpWithdrawInput.value = '';
 }
 function handleWithdraw() {
@@ -159,10 +228,25 @@ function handleWithdraw() {
         return;
     }
 
-    balance -= amount;
     totalWithdraw += amount;
-    updateBalance(balance);
+    updateBalance(balance - amount);
     topUpWithdrawInput.value = '';
+}
+function hashTimeFrame(timeFrame) {
+    if (timeFrame == "TIME_SERIES_INTRADAY_EXTENDED") {
+        return "15 min";
+    }
+    else if (timeFrame == "TIME_SERIES_DAILY_ADJUSTED") {
+        return "Day";
+    }
+    else if (timeFrame == "TIME_SERIES_WEEKLY") {
+        return "Week";
+    }
+    else if (timeFrame == "TIME_SERIES_MONTHLY") {
+        return "Month";
+    } else {
+        return "";
+    }
 }
 
 
@@ -201,6 +285,159 @@ if (tabs) {
 }
 
 
+
+// Trade Part
+$(document).ready(function() {
+    const apiKey = "C3XZTDGXRR6K8AZS";
+    const searchInput = $("#searchInput");
+    const searchResults = $("#searchResults");
+    let selectedStock = "";
+
+    // Function to fetch search results from Alpha Vantage API
+    function fetchSearchResults(keyword) {
+        const url = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=${apiKey}`;
+        
+        // Make the API request
+        $.get(url, function(data) {
+            searchResults.empty();
+            const results = data.bestMatches;
+            if (!results) return;
+            results.forEach(function(result) {
+                const symbol = result["1. symbol"];
+                const name = result["2. name"];
+                // const listItem = `<li>${symbol} - ${name}</li>`;
+                const listItem = `<option><p>${symbol} - ${name}</p></option>`;
+                const $listItem = $(listItem);
+                
+                // Add click event handler to the autocomplete item
+                $listItem.on("click", function() {
+                    // Update the input value with the selected option
+                    searchInput.val(symbol);
+                    
+                    // Perform desired action when an item is clicked
+                    console.log("Selected:", symbol);
+                    selectedStock = `${symbol} - ${name}`;
+                    // You can use the selected symbol or perform any other action here
+                    searchResults.empty();
+                });
+                
+                searchResults.append($listItem);
+            });
+        });
+    }
+
+    // Event handler for input changes
+    var delay = null;
+    function search() {
+        clearTimeout(delay);
+
+        const keyword = searchInput.val();
+        delay = setTimeout(function(){fetchSearchResults(keyword);}, 300);
+    }
+    searchInput.keyup(search)
+    searchInput.focusin(search);
+
+    var focusOutDelay = null;
+    searchInput.focusout(() => {
+        clearTimeout(focusOutDelay);
+        focusOutDelay = setTimeout(function(){searchResults.empty();}, 300);
+    })
+
+
+
+
+
+    // Event handler for "View Chart" button click
+    $("#view-chart-button").click(function() {
+        // Get the selected stock and time frame
+        const selectedStock = $("#searchInput").val();
+        const selectedTimeFrame = $("#Time-Frame").val();
+
+        // Store the values in localStorage
+        localStorage.setItem("selectedStock", selectedStock);
+        localStorage.setItem("selectedTimeFrame", selectedTimeFrame);
+
+        // Redirect to the other HTML file
+        window.open("visualization.html");
+    });
+
+    $("#confirm-order-button").click(function() {
+        let stock = selectedStock;
+        let timeFrame = $("#Time-Frame").val();
+        let size = $("#trade-options-size").val();
+        let price = $("#trade-options-price").val();
+        let stopLoss = $("#trade-options-stoploss").val();
+        let takeProfit = $("#trade-options-takeprofit").val();
+        let type = $("#stock-type").val();
+
+        function resetInputs() {
+            $("#searchInput").val('');
+            $("#Time-Frame").val('');
+            $("#trade-options-size").val('');
+            $("#trade-options-price").val('');
+            $("#trade-options-stoploss").val('');
+            $("#trade-options-takeprofit").val('');
+            $("#stock-type").val('');
+        }
+
+        const invalid = (!stock) || (!size) || (!price) || (!stopLoss) || (!takeProfit);
+
+        if (invalid) {
+            alert("Please fill up all the fields!");
+            return;
+        }
+        
+        timeFrame = hashTimeFrame(timeFrame);
+
+        (async () => {
+            const portfolioDocRef = doc(db, userEmail, "Portfolio");
+
+            await (async () => {
+                const docSnap = await getDoc(portfolioDocRef);
+                const docData = await docSnap.data();
+                const balance = await docData.balance;
+
+                if (balance - size * price < 0) {
+                    alert("Insufficient funds. This order has exceeded the current balance");
+                    return;
+                }
+
+                await (async () => updateBalance(balance - size * price))();
+
+                await updateDoc(portfolioDocRef, {
+                    "numOfStocks": ++numOfStocks
+                });
+
+                await setDoc(doc(db, userEmail, "Portfolio", "Stocks", stock), {
+                    "stock": stock,
+                    "timeFrame": timeFrame,
+                    "size": size,
+                    "price": price,
+                    "stopLoss": stopLoss,
+                    "takeProfit": takeProfit,
+                    "type": type 
+                });
+
+                alert("Order is Placed!");
+                console.log("Order is Placed!");
+
+                // RESET ALL INPUTS
+                resetInputs();
+
+                // await initializePortfolioTable();
+            })();
+
+
+        })();
+
+    });
+    
+
+});
+
+
+
+
 // settingsPart
 
 if (changePasswordBtn) {
@@ -219,7 +456,8 @@ function logOut() {
     signOut(auth).then(() => {
         // Sign-out successful.
         alert('User logged out!');
-        window.location.assign("index.html");
+        localStorage.clear();
+        window.location.assign("login.html");
     }).catch((error) => {
         // An error happened.
     });
@@ -307,6 +545,6 @@ if (backBtn) {
 
 
 
-window.addEventListener('beforeunload', () => {
-    sessionStorage.clear();
-});
+// window.addEventListener('beforeunload', () => {
+//     sessionStorage.clear();
+// });
